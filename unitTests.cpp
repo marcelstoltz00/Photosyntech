1e811ffca8586e5a449ac1d7b1f827fb450391bb
Wilmar-Smit <u24584216@tuks.co.za>
2025-10-20 18:13:52 +0200

    will be fixed

--- unitTests.cpp
+++ unitTests.cpp
@@ -1,6 +1,8 @@
 // please use testing like this.
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "flyweight/Flyweight.h"
 #include "flyweight/FlyweightFactory.h"
+#include "doctest.h"
 void SingleTonTests()
 {
 }
