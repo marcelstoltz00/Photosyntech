01f441469cb6f2f6849c8da0dd75d5a79b94e2fb
Wilmar-Smit <u24584216@tuks.co.za>
2025-10-20 18:05:48 +0200

    fly weight deconstructor

--- flyweight/FlyweightFactory.cpp
+++ flyweight/FlyweightFactory.cpp
@@ -16,7 +16,21 @@ Flyweight<T> *FlyweightFactory<ID, T>::getFlyweight(ID id, T data)
     {
         if (data != NULL)
             cache[id] = new Flyweight<T>(data);
-            
+
         return cache[id];
+    }
+}
+
+template <class ID, class T>
+FlyweightFactory<ID, T>::~FlyweightFactory()
+{
+    typename unordered_map<ID, T>::iterator itr = cache.begin();
+    while (itr != cache.end())
+    {
+        if (*itr)
+        {
+            delete *itr->second();
+        }
+        itr++;
     }
 }
