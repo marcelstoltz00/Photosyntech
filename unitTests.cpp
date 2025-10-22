59e37b471e06edd628bbeba147fd08864d185851
Wilmar-Smit <u24584216@tuks.co.za>
2025-10-22 07:46:06 +0200

    updated testing

--- unitTests.cpp
+++ unitTests.cpp
@@ -13,16 +13,24 @@ TEST_CASE("Overall Testing of flyweight strings + error handling")
 {
     FlyweightFactory<int, string *> *fac = new FlyweightFactory<int, string *>();
 
-    CHECK(fac != nullptr);
+    SUBCASE("factory is created")
+    {
+        CHECK(fac != nullptr);
+    }
 
-    fac->getFlyweight(0, new string("Insert1"));
-    CHECK(*fac->getFlyweight(0)->getState() == "Insert1");
+    SUBCASE("Correct String is inserted and accessable")
+    {
+        fac->getFlyweight(0, new string("Insert1"));
+        CHECK(*fac->getFlyweight(0)->getState() == "Insert1");
+    }
 
-    fac->getFlyweight(1, new string("Insert2"));
-    CHECK(*fac->getFlyweight(1)->getState() == "Insert2");
-
-    CHECK(*fac->getFlyweight(0)->getState() == "Insert1");
-
+    SUBCASE("Correct String is inserted and when multiple strings exist")
+    {
+        fac->getFlyweight(0, new string("Insert1"));
+        fac->getFlyweight(1, new string("Insert2"));
+        CHECK(*fac->getFlyweight(1)->getState() == "Insert2");
+        CHECK(*fac->getFlyweight(0)->getState() == "Insert1");
+    }
     try
     {
         fac->getFlyweight(5);
@@ -50,6 +58,7 @@ TEST_CASE("Overall Testing of flyweight water strategies")
 }
 TEST_CASE("Singleton basics with water strategy testing and with state testing")
 {
+
     LivingPlant *plant = new Tree();
 
     Inventory *inv = Inventory::getInstance();
@@ -63,9 +72,12 @@ TEST_CASE("Singleton basics with water strategy testing and with state testing")
     };
     CHECK(plant->getAge() == 7);
 
-    CHECK(Inventory::getInstance()->getWaterFly(5000)->getState()->water(plant) == 10);
+    SUBCASE("Correct exception handling")
+    {
+        CHECK(Inventory::getInstance()->getWaterFly(5000)->getState()->water(plant) == 10);
 
-    CHECK(*Inventory::getInstance()->getString("Insert1")->getState() == "Insert1");
+        CHECK(*Inventory::getInstance()->getString("Insert1")->getState() == "Insert1");
+    }
 
     delete inv;
     delete plant;
