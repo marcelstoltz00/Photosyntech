5399bf590566762d88218d907c94239546558c5f
Wilmar-Smit <u24584216@tuks.co.za>
2025-10-22 17:02:51 +0200

    Testing update as well as improved getInfo

--- prototype/LivingPlant.cpp
+++ prototype/LivingPlant.cpp
@@ -125,8 +125,9 @@ std::string LivingPlant::getInfo()
     baseInfo += "Water Level: " + std::to_string(waterLevel) + "\n";
     baseInfo += "Sun Exposure: " + std::to_string(sunExposure) + "\n";
     baseInfo += "Base Price: R" + std::to_string(price) + "\n";
-    baseInfo += "Water Affection: " + std::to_string(affectWaterValue) + "\n";
-    baseInfo += "Sun Affection: " + std::to_string(affectSunValue) + "\n";
+    baseInfo += "Total Price: R" + std::to_string(decorator->getPrice()) + "\n";
+    baseInfo += "Water Affection: " + std::to_string(decorator->affectWater()) + "\n";
+    baseInfo += "Sun Affection: " + std::to_string(decorator->affectSunlight()) + "\n";
 
     return baseInfo;
 };
@@ -249,7 +250,6 @@ PlantComponent *LivingPlant::getDecorator()
 {
     return this->decorator;
 }
-
 
 PlantComponent *LivingPlant::correctShape(PlantComponent *mainDecorator)
 {
