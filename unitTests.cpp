eca4fce1175327f91bdc8b08684c59101b668ad4
jcoet-gh <johancoet9@gmail.com>
2025-10-22 11:03:55 +0200

    Pasted Wilmar's testing

--- unitTests.cpp
+++ unitTests.cpp
@@ -6,31 +6,12 @@
 #include "strategy/MidWater.h"
 #include "singleton/Singleton.h"
 #include <doctest/doctest.h>
-
 #include "prototype/Tree.h"
-#include "prototype/Shrub.h"
-#include "prototype/Herb.h"
 #include "state/Seed.h"
-
-#include "prototype/LivingPlant.h"
-#include "decorator/customerDecorator/PlantDecorationHeader.h"
-#include "decorator/plantDecorator/PlantAttributesHeader.h"
-/*
-#include "composite/PlantGroup.h"
-
-
 
 TEST_CASE("Overall Testing of flyweight strings + error handling")
 {
     FlyweightFactory<int, string *> *fac = new FlyweightFactory<int, string *>();
-<<<<<<< HEAD
-    CHECK(fac != nullptr);
-    fac->getFlyweight(0, new string("Insert1"));
-    CHECK(*fac->getFlyweight(0)->getState() == "Insert1");
-    fac->getFlyweight(1, new string("Insert2"));
-    CHECK(*fac->getFlyweight(1)->getState() == "Insert2");
-    CHECK(*fac->getFlyweight(0)->getState() == "Insert1");
-=======
 
     SUBCASE("factory is created")
     {
@@ -50,7 +31,6 @@ TEST_CASE("Overall Testing of flyweight strings + error handling")
         CHECK(*fac->getFlyweight(1)->getState() == "Insert2");
         CHECK(*fac->getFlyweight(0)->getState() == "Insert1");
     }
->>>>>>> singleton-Flyweight
     try
     {
         fac->getFlyweight(5);
@@ -58,44 +38,39 @@ TEST_CASE("Overall Testing of flyweight strings + error handling")
     catch (const char *e)
     {
         std::cerr << e << '\n';
-
-        delete fac;
     }
+
+    delete fac;
 }
 TEST_CASE("Overall Testing of flyweight water strategies")
 {
     FlyweightFactory<int, WaterStrategy *> *fac = new FlyweightFactory<int, WaterStrategy *>();
     LivingPlant *plant = new Tree();
     fac->getFlyweight(LowWater::getID(), new LowWater());
+
     CHECK(fac->getFlyweight(LowWater::getID())->getState()->water(plant) == 10);
+
     fac->getFlyweight(MidWater::getID(), new MidWater());
+
     CHECK(fac->getFlyweight(MidWater::getID())->getState()->water(plant) == 20);
     delete fac;
     delete plant;
 }
-*/
 TEST_CASE("Singleton basics with water strategy testing and with state testing")
 {
-<<<<<<< HEAD
-    Inventory *inv = Inventory::getInstance();
-=======
->>>>>>> singleton-Flyweight
 
     LivingPlant *plant = new Tree();
 
+    Inventory *inv = Inventory::getInstance();
     CHECK(inv == Inventory::getInstance());
     CHECK(inv->getWaterFly(LowWater::getID())->getState()->water(plant) == 10);
     CHECK(Inventory::getInstance()->getWaterFly(MidWater::getID())->getState()->water(plant) == 20);
+
     for (int i = 0; i < 7; i++)
     {
         Inventory::getInstance()->getStates(Seed::getID())->getState()->grow(plant);
     };
-
     CHECK(plant->getAge() == 7);
-<<<<<<< HEAD
-    CHECK(Inventory::getInstance()->getWaterFly(5000)->getState()->water(plant) == 10);
-    CHECK(*Inventory::getInstance()->getString("Insert1")->getState() == "Insert1");
-=======
 
     SUBCASE("Correct exception handling")
     {
@@ -103,105 +78,17 @@ TEST_CASE("Singleton basics with water strategy testing and with state testing")
 
         CHECK(*Inventory::getInstance()->getString("Insert1")->getState() == "Insert1");
     }
->>>>>>> singleton-Flyweight
 
     delete plant;
+
     delete inv;
 }
 
 TEST_CASE("Testing decorator")
 {
- 
-}
+    LivingPlant *plant = new Tree();
 
-// TEST_CASE("Testing composite")
-// {
-    // PlantComponent *tree = new Tree();
-    // PlantComponent *shrub = new Shrub();
-    // PlantGroup *group1 = new PlantGroup();
-
-    // group1->addComponent(tree);
-    // group1->addComponent(shrub);
-
-    // CHECK(group1->getPrice() == 225.00); 
-
-    // CHECK(group1->affectWater() == 9);  
-
-    // CHECK(group1->affectSunlight() == 9);
-
-    // PlantGroup *group2 = dynamic_cast<PlantGroup*>(group1->clone());
-
-    // CHECK(group2 != nullptr);
-    // CHECK(group2->getPrice() == 225.00);
-
-    // LivingPlant *herb = new Herb();
-    // group1->addComponent(herb); 
-
-    // CHECK(group1->getPrice() == 255.00);
-
-    // CHECK(group1->affectWater() == 12);
-
-    // CHECK(group2->getPrice() == 225.00);
-
-    // CHECK(group2->affectWater() == 9);
-
-    // delete group1;
-    // delete group2;
-// }
-
-// testing decorator
-/*
-#include <iostream>
-
-
-int main()
-{
-    PlantComponent *myPlant = new LargePot();
-
-    myPlant->addAttribute(new ShopThemedCharm());
-
-    LivingPlant *herb = new LivingPlant("Herb", 30.00, 3, 3);
-
-    herb->setWaterStrategy(2);
-    herb->setSunStrategy(2);
-
-    myPlant->addAttribute(herb);
-
-    myPlant->water();
-    myPlant->setOutside();
-
-    myPlant->affectWater();
-    myPlant->affectSunlight();
-
-    std::cout << "Current Plant Info:\n"
-              << myPlant->getInfo() << std::endl;
-    std::cout << "Current Price: R" << myPlant->getPrice() << std::endl;
-    std::cout << "----------------------------------" << std::endl;
-    std::cout << "Drought incoming, pasop oaks" << std::endl;
-
-    for (int i = 0; i < 5; i++)
-    {
-        myPlant->affectWater();
-    }
-
-    std::cout << "Plant Info After Drought:\n"
-              << myPlant->getInfo() << std::endl;
-    std::cout << "Price: R" << myPlant->getPrice() << std::endl;
-    std::cout << "----------------------------------" << std::endl;
-
-    std::cout << "Infinite darkness. bruh." << std::endl;
-    for (int i = 0; i < 5; i++)
-    {
-        myPlant->affectSunlight();
-    }
-
-    std::cout << "Plant Info After Darkness:\n"
-              << myPlant->getInfo() << std::endl;
-    std::cout << "Price: R" << myPlant->getPrice() << std::endl;
-    std::cout << "----------------------------------" << std::endl;
-
-    delete myPlant;
+    
     delete Inventory::getInstance();
-
-    return 0;
-*/
+    delete plant;
+}
